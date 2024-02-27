56989499046aec00e4da5523c148b8a637a80498
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-27 08:04:56 -0300

    feat: :lipstick: mock header and profile menu

--- components/Ui/Header.vue
+++ components/Ui/Header.vue
@@ -4,11 +4,18 @@ import { BASE_TITLE } from "~/constants/siteInfos"
 
 <template>
   <header class="border-b-2 border-slate-200 py-4">
-    <UContainer>
+    <UContainer class="flex items-center gap-3 flex-wrap justify-between">
       <NuxtLink to="/" class="flex items-center gap-2">
         <img src="/logo.svg" width="30" height="30" />
-        <span class="font-semibold text-2xl">{{ BASE_TITLE }}</span>
+        <span class="font-semibold text-xl md:text-2xl">{{ BASE_TITLE }}</span>
       </NuxtLink>
+
+      <div class="flex items-center gap-4">
+        <UChip size="lg">
+          <UButton icon="i-heroicons-bell-alert" color="gray" />
+        </UChip>
+        <ProfileMenu />
+      </div>
     </UContainer>
   </header>
 </template>
