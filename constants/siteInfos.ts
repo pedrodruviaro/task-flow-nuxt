5ec321db6c88e8cea4f027cd22bcd981f3502145
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-29 10:52:29 -0300

    feat: :sparkles: auth layout and base site description

--- constants/siteInfos.ts
+++ constants/siteInfos.ts
@@ -1,1 +1,3 @@
 export const BASE_TITLE = "TaskFlow"
+export const BASE_DESCRIPTION =
+  "Streamline project management effortlessly. Organize, prioritize, and track tasks seamlessly with our intuitive web app."
--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import { BASE_TITLE, BASE_DESCRIPTION } from "@/constants/siteInfos"
+
+definePageMeta({
+  layout: "auth",
+})
+
 const supabase = useSupabaseClient()
 
 async function handleLogin() {
@@ -21,14 +27,27 @@ async function logout() {
 </script>
 
 <template>
-  <div>
-    <h2>Login with google</h2>
-    <UButton
-      @click="handleLogin"
-      trailing-icon="i-heroicons-arrow-right-end-on-rectangle"
-      size="lg"
-      >Login</UButton
-    >
-    <!-- <UButton @click="logout">Logout</UButton> -->
-  </div>
+  <section>
+    <div class="flex flex-col justify-center text-center">
+      <div class="flex items-center gap-4 mx-auto mb-6">
+        <img src="/logo.svg" width="60" height="60" />
+        <span class="font-semibold text-4xl md:text-6xl">{{ BASE_TITLE }}</span>
+      </div>
+      <h1 class="max-w-[50ch] text-xl">
+        {{ BASE_DESCRIPTION }}
+      </h1>
+    </div>
+
+    <UCard class="max-w-max mx-auto mt-10">
+      <h2 class="text-center">Enter with your Google account</h2>
+      <UButton
+        @click="handleLogin"
+        trailing-icon="i-heroicons-arrow-right-end-on-rectangle"
+        size="xl"
+        class="mx-auto flex mt-4"
+        >Login</UButton
+      >
+      <!-- <UButton @click="logout">Logout</UButton> -->
+    </UCard>
+  </section>
 </template>
